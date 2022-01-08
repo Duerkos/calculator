49d0d62c4c740c2f93471ac8b7183cb542d30d1c
Duerkos <vicentearcedavid@gmail.com>
2022-01-08 14:18:59 +0100

    Added basic html page with no buttons and basic operators

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.querySelector(".displayDiv");
+
+function add(a,b){return a+b;} 
+function substract(a,b){return a-b;}
+function multiply(a,b){return a*b;}
+function divide(a,b){return a/b;}
+
+function operate(operator,a,b){
+    switch (operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return substract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
+function updateDisplay(number){
+    display.textContent = number;
+}
+
+
