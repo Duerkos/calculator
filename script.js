cf3a56732ff489884b3d1965ab5ee0721905c4b0
Duerkos <vicentearcedavid@gmail.com>
2022-01-09 10:08:48 +0100

    Add buttons for basic calculator, display updates with digits

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const display = document.querySelector(".displayDiv");
+let input = "";
 
 function add(a,b){return a+b;} 
 function substract(a,b){return a-b;}
@@ -17,8 +18,21 @@ function operate(operator,a,b){
             return divide(a,b);
     }
 }
+
+function addDigits(digit){
+    input = input + digit;
+    updateDisplay(input);
+}
+
 function updateDisplay(number){
     display.textContent = number;
 }
+const buttonDigits = document.querySelectorAll(".buttonDigit");
+const buttonOperations = document.querySelectorAll(".buttonOps");
 
+buttonDigits.forEach((buttonDigit) => {
+    buttonDigit.addEventListener("click", () =>{
+        addDigits(buttonDigit.id);
+});
 
+});
